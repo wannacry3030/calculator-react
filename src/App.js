9be44fb71a3f7f5fe7bba61d1f253880c5493442
wannacry3030 <wannacry.bat@proton.me>
2023-05-10 15:48:49 -0300

    update App.js and src/index.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import React, { useState } from "react";
 
 const btnValues = [
   ["C", "+-", "%", "/"],
@@ -12,9 +13,15 @@ const btnValues = [
 ];
 
 const App = () => {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
   return (
     <Wrapper>
-      <Screen value={0} />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
           return (
