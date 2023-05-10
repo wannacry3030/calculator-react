3dc925fbbdbee61dda87b76a79d8ec5c7a3751c9
wannacry3030 <wannacry.bat@proton.me>
2023-05-10 14:45:17 -0300

    update Button.js and ButtonBox.css

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import "./Button.js";
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Button;
